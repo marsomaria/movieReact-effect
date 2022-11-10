3ee5c8c3e211ba138dbb5baed7e4ace8083fd313
marsomaria <marsomaria5@gmail.com>
2022-11-10 19:54:51 +0100

    first

--- moviemax/src/App.js
+++ moviemax/src/App.js
@@ -2,11 +2,16 @@
 import './App.css';
 import {useEffect, useState} from "react";
 import Movie from "./Movie.js";
+import Filter from "./Filter.js"
+import {motion, AnimatePresence } from "framer-motion";
 
+//https://developers.themoviedb.org/3/genres/get-movie-list
 
 function App() {
   const [popular, setPopular]=useState([]);
-  
+  const [filtered, setFiltered]= useState([]);
+  const [activeGenre , setActiveGenre] = useState(0);
+
   useEffect(()=>{
     fetchPopular();
   },[]);
@@ -21,16 +26,45 @@ function App() {
     const movies = await data.json();
     setPopular(movies.results);
     console.log(movies.results);
- 
+    setFiltered(movies.results);
   }
 
   return (
     <div className="App">
-      <div className="popular-movies">
-        {popular.map(movie => {
-          return <Movie key={movie.id} movie={movie}/>
-        })}
-      </div>
+      <Filter popular={popular} setFiltered={setFiltered} activeGenre={activeGenre} setActiveGenre={setActiveGenre}></Filter>
+      
+      {/* we want to animate the grid so we have 
+      to change div 
+      to motion.div to have acces like animate
+      animate={{ y:100 }}  
+      
+      with layout uf we have problems with 
+      the visualitzation,
+      we should put the layout on the child div too
+      
+      layout also works with display:flex
+      
+      for fade out and fade in the movie 
+      when they disappear whe can do on the div child->
+      for fade in-> animate={{ opacity:1}} initial={{ opacity:0}}
+
+      for fade out exit={{ opacity:0 }}
+
+      also we need to wrappe the motion div on a animatepresence
+      
+
+      now they firs dissappear and then the movies were reorganized
+      we can also determine the time that the transition is going to work
+      
+      */}
+
+      <motion.div layout className="popular-movies">
+        <AnimatePresence>
+          {filtered.map(movie => {
+            return <Movie key={movie.id} movie={movie}/>
+          })}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 }
--- moviemax/src/Movie.js
+++ moviemax/src/Movie.js
@@ -1,10 +1,27 @@
+import { motion } from "framer-motion";
+
+
 function Movie({movie}){
     return (
-        // <div className="popular-movies">
-        <div >
+        // lets show whatever we want of the movie
+        // for example title and img
+
+
+    /* for fade out and fade in the movie 
+      when they disappear whe can do on the div child->
+      for fade in-> animate={{ opacity:1}} initial={{ opacity:0}}
+
+      for fade out exit={{ opacity:0 }} and also another component
+        of frame motion called 
+      also we need to wrappe
+      
+we can also determine the time that the transition is going to work
+*/
+
+        <motion.div animate={{ opacity:1 }} initial={{ opacity:0 }} exit={{ opacity:0 }} transition={{duration:2}} layout>
             <h2>{movie.title}</h2>
             <img src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} alt=""></img>
-        </div>
+        </motion.div>
     )
 }
 export default Movie;
